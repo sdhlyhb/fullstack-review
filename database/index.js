8aa8088295b2aaff78e328fff425b2eeb0d99838
Danting Huang <serena.huangdanting@gmail.com>
2022-05-26 03:17:45 -0400

    setting up. update helper fn, database files

--- database/index.js
+++ database/index.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher', { 'useNewUrlParser': true, 'useUnifiedTopology': true, 'useFindAndModify': false, 'useCreateIndex': true});
 
-let repoSchema = mongoose.Schema({
+let repoSchema = new mongoose.Schema({
   // TODO: your schema here!
+  repo_id: {type: Number, unique: true, required: true},
+  user_id: Number,
+  repo_url: String,
+  repo_name: String,
+  username: String,
+  size: Number,
+  stargazers_count: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (dataArr) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  return Repo.insertMany(dataArr)
+    .then(() => console.log('Sucess save the data to database!'))
+    .catch(err => console.log('Err! Failed to save the data to the database!'))
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 let app = express();
+const helper = require('../helpers/github.js');
+const db = require('../database/index.js');
 
+
+
+app.use(express.json());
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos', function (req, res) {
@@ -8,6 +13,7 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  console.log('this is the req.body:',  req.body);
 });
 
 app.get('/repos', function (req, res) {
