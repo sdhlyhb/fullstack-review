acc95d2fce295656af1c76b2aa0fb660bf4ff58d
Danting Huang <serena.huangdanting@gmail.com>
2022-06-03 00:44:40 -0400

    another edit

--- client/src/components/UserEntry.jsx
+++ client/src/components/UserEntry.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const UserEntry = (props) => {
+  var avatarUrl = "https://github.com/" + props.user + ".png?size=20";
   return (
-    <li>
+    <li className="userEntry">
+      <img src={avatarUrl}/>
+      {" "}
       <a>{props.user}</a>
     </li>
 
